704770705f4c7c2d6798a4fb09a151e79c946339
akaci <akaci@email.com>
2024-03-27 18:16:48 +0100

    Initial commit

---
+++ motor.py
@@ -0,0 +1,30 @@
+from logger import LOGGER
+from periphery import GPIO
+import time
+
+
+class Motor:
+    def __init__(self, pulse_pin, direction_pin, steps):
+        self.pulse_duration = 0.001
+        self.dead_duration = 1
+
+        self.pulse_pin = GPIO(pulse_pin, "out")
+        self.direction_pin = GPIO(direction_pin, "out")
+        self.steps = steps
+        LOGGER.debug("Motor initialized")
+
+    def turn_motor(self, pulse_duration):
+        LOGGER.debug("Start turning motor")
+        self.pulse_pin.write(True)
+        time.sleep(pulse_duration)
+
+    def hold_motor(self, hold_duration):
+        LOGGER.debug("Stop turning motor")
+        self.pulse_pin.write(False)
+        time.sleep(hold_duration)
+
+    def turn_off(self):
+        self.pulse_pin.write(False)
+        self.pulse_pin.close()
+        self.direction_pin.close()
+        LOGGER.debug("Motor turned off")
